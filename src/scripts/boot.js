099100a93d2d57d7eeb78fd4fdaf7a941622696e
Anthony Edwards <taacqan@hotmail.co.uk>
2025-11-20 16:36:10 +0000

    Update boot/reboot to use clean server script

--- src/scripts/boot.js
+++ src/scripts/boot.js
@@ -1,28 +1,5 @@
-import { scan_for_servers } from "/src/scripts/util/scan_for_servers"
 import { PORT_NAMES } from "/src/scripts/manage_ports"
-
-/**
- * @param {import("@ns").NS} ns
- */
-function kill_all_other_processes(ns) {
-  let rooted_servers = scan_for_servers(ns,{"is_rooted":true,"include_home":true})
-  let cnt = 0
-
-  ns.tprint(`INFO: Killing all other processes.`)
-  // Kill all possible actions on all servers apart from this process
-  for (let server of rooted_servers) {
-    let process_ids = ns.ps(server)
-    for (let process of process_ids) {
-      // Do not kill our own process
-      if (process.pid == ns.pid) {
-        continue
-      }
-      ns.kill(process.pid)
-      cnt++
-    }
-  }
-  ns.tprint(`INFO: We killed ${cnt} processes during booting.`)
-}
+import { kill_all_other_processes } from "/src/scripts/util/static/kill_all_other_processes"
 
 /** @param {import("@ns").NS} ns */
 async function setup_port_ids(ns) {
--- src/scripts/reboot.js
+++ src/scripts/reboot.js
@@ -1,28 +1,6 @@
-import { scan_for_servers } from "/src/scripts/util/scan_for_servers"
 import { PORT_IDS } from "/src/scripts/util/constant_utilities"
-    
-/**
- * @param {import("@ns").NS} ns
- */
-function kill_all_other_processes(ns) {
-  let rooted_servers = scan_for_servers(ns,{"is_rooted":true,"include_home":true})
-  let cnt = 0
-  // This function is called soon after control_v4.js is started.
-  // We should have a clean slate to build from, so kill all possible actions on all servers apart from this process
-  for (let server of rooted_servers) {
-    let process_ids = ns.ps(server)
-    for (let process of process_ids) {
-      // Do not kill our own process
-      if (process.pid == ns.pid) {
-        continue
-      }
-      ns.kill(process.pid)
-      cnt++
-    }
-  }
+import { kill_all_other_processes } from "/src/scripts/util/static/kill_all_other_processes"
 
-  ns.tprint("We killed " + cnt + " processes during reboot.")
-  }
 
 /** @param {import("@ns").NS} ns */
 export async function main(ns) {
@@ -36,8 +14,8 @@ export async function main(ns) {
 
   if (arg_flags.all) {
     kill_all_other_processes(ns)
-    let filename = "/scripts/control_v4.js"
-    ns.exec(filename, "home")
+    let filename = "/scripts/boot.js"
+    ns.run(filename)
   }
 
   if (arg_flags.gang) {
