d12cbd8f7d1d4a9e94746c47aa1f5a89c5066afd
Anthony Edwards <taacqan@hotmail.co.uk>
2025-12-02 21:49:18 +0000

    Clean up

--- src/scripts/manage_hacking_v5.js
+++ src/scripts/manage_hacking_v5.js
@@ -213,181 +213,6 @@ function update_TUI(ns, process_info, force_update) {
 }
 
 /**
- * @param {import("@ns").NS} ns
- */
-async function check_manage(ns, control_params, bitnode_mults, server_info) {
-
-  let managed_servers = []
-  let preping_servers = []
-  for (let server in RAM_INFO) {
-    for (let pid in RAM_INFO[server].processes) {
-      if (ns.isRunning(parseInt(pid))) {
-        //log(ns, "Live PID: " + pid)
-        switch (RAM_INFO[server].processes[pid].filename) {
-          case "/scripts/manage_server_hack_v3.js":
-            managed_servers.push(RAM_INFO[server].processes[pid].target)
-            break
-          case "/scripts/manage_server_prep_v3.js":
-            preping_servers.push(RAM_INFO[server].processes[pid].target)
-            break
-        }
-      }
-      else {
-        //log(ns, "Killed PID " + pid + " as it is not running.")
-        let response = await release_ram(ns, server, RAM_INFO[server].processes[pid].ram_cost)
-        //log(ns, "RAM Release Response: " + response)
-        if (!(response.result === "OK")) {
-          ns.tprint("ERROR PID " + pid + " is dead, but we failed to release the RAM associated with it.")
-        }
-        delete RAM_INFO[server].processes[pid]
-      }
-    }
-  }
-
-  // log(ns, managed_servers.length + " Managed Server processes found")
-  // log(ns, preping_servers.length + " Prepare Server processes found")
-
-  let servers = Object.keys(server_info).filter(server => server_info[server].is_rooted && server_info[server].max_money > 0)
-
-  let temp_server = []
-  if (bitnode_mults["ScriptHackMoneyGain"] === 0) {
-    // We gain no money from hacking. We only gain the side effects from hacks being run
-    // Do not start any managers or prepers
-    servers = []
-  }
-  else if (server_info["home"].max_ram < control_params.hacker.consider_early) {
-    if (servers.includes("n00dles")  && server_info["n00dles"].is_rooted) temp_server.push("n00dles")
-    if (servers.includes("joesguns") && server_info["joesguns"].is_rooted) temp_server.push("joesguns")
-    if (servers.includes("phantasy") && server_info["phantasy"].is_rooted) temp_server.push("phantasy")
-    servers = temp_server
-  }
-
-  servers.sort(
-    function(a,b){
-      let player = ns.getPlayer()
-      let server_a = ns.getServer(a)
-      let server_b = ns.getServer(b)
-      server_a.hackDifficulty = server_a.minDifficulty
-      server_a.moneyAvailable = server_a.moneyMax
-      server_b.hackDifficulty = server_b.minDifficulty
-      server_b.moneyAvailable = server_b.moneyMax
-
-      let server_a_hack_percent = ns.formulas.hacking.hackPercent(server_a, player)
-      let server_a_hack_chance  = ns.formulas.hacking.hackChance (server_a, player)
-      let server_b_hack_percent = ns.formulas.hacking.hackPercent(server_b, player)
-      let server_b_hack_chance  = ns.formulas.hacking.hackChance (server_b, player)
-
-      return (server_b_hack_percent * server_b_hack_chance * server_b.moneyMax)
-      - (server_a_hack_percent * server_a_hack_chance * server_a.moneyMax)
-    }
-  )
-
-  // TODO: Narrow down prep managers to only prep the single most profitable un-preped server at a time.
-
-  let hackable_servers = []
-  let servers_to_prep = []
-
-  for (let server of servers) {
-    if (
-      (
-          ns.getServerSecurityLevel(server) > server_info[server].min_diff
-      ||  ns.getServerMoneyAvailable(server) < server_info[server].max_money
-      )
-    &&  managed_servers.indexOf(server) == -1
-    ) {
-      // We are not Managing this server AND it is not in a preped state.
-      // Add to the list of servers to prep.
-      // log(ns, `Server to prep ${server} as:`)
-      // log(ns, `Sec Level of Server: ${ns.getServerSecurityLevel(server)} != ${server_info[server].min_diff}`)
-      // log(ns, `Money Level of Server: ${ns.getServerMoneyAvailable(server)} != ${server_info[server].max_money}`)
-      servers_to_prep.push(server)
-      continue
-    }
-    else if (
-      preping_servers.indexOf(server) != -1
-    ) {
-      // We are preping this server. Do not add it to hackable servers yet.
-      continue
-    }
-    hackable_servers.push(server)
-  }
-
-  let servers_to_hack = []
-  let hack_batch_cnt = 0
-
-  for (let server of hackable_servers){
-    let batches_to_saturate_server = Math.max(Math.floor(ns.getWeakenTime(server) / control_params.hacker.hack_batch_time_interval), 1)
-    if (hack_batch_cnt + batches_to_saturate_server < control_params.hacker.total_hack_batch_limit) {
-      // log(ns, "Added " + server + " with " + batches_to_saturate_server + " to the list of servers we will hack")
-      hack_batch_cnt += batches_to_saturate_server
-      servers_to_hack.push(server)
-    }
-  }
-  // log(ns, "Total of " + hack_batch_cnt + " hack batches are expected to be spawned")
-
-  for (let server of managed_servers) {
-    if (servers_to_hack.indexOf(server) === -1) {
-      // log(ns, "Killing manager for " + server + " as we no longer want to hack it.")
-      await kill_child(ns, server)
-    }
-  }
-
-  for (let server of servers_to_hack) {
-    // Not managing currently, request new manager
-    let successful = false
-    if (managed_servers.indexOf(server) == -1) {
-      // log(ns, "Launching manager for " + server + ".")
-      successful = await launch_child(ns, "/scripts/manage_server_hack_v3.js", server)
-      if (!successful) {
-        // log(ns, "Failed to launch manager for " + server + ".")
-      }
-    }
-  }
-
-  if (!(control_params.hacknet_mgr.hash_target === undefined)) {
-    if (!(managed_servers.includes(control_params.hacknet_mgr.hash_target) || preping_servers.includes(control_params.hacknet_mgr.hash_target))) {
-      for (let server of preping_servers) {
-        // log(ns, `Killing preper for ${server} as we need space to prep the hash target`)
-        await kill_child(ns, server)
-        // log(ns, `Server prepper killed`)
-      }
-      servers_to_prep = []
-      // log(ns, `Overrideing servers to prep to prep hash target ${control_params.hacknet_mgr.hash_target}`)
-      servers_to_prep.push(control_params.hacknet_mgr.hash_target)
-    }
-  }
-
-  // // This is not working when the hash target is being hacked currently
-  // if (!preping_servers.includes(control_params.hacknet_mgr.hash_target) && !managed_servers.includes(control_params.hacknet_mgr.hash_target)) {
-  //   for (let server of preping_servers) {
-  //     log(ns, "Killing preper for " + server + " as we need space to prep the hash target")
-  //     await kill_child(ns, server)
-  //   }
-  // }
-
-  for (let server of servers_to_prep) {
-    // Not preping currently, request new prepper
-    let successful = false
-    //ns.print("Server: " + server + ". P_S: " + preping_servers.length + ". Inclues: " + preping_servers.includes(server))
-    if (
-        preping_servers.length < 10
-    &&  !preping_servers.includes(server)
-    ) {
-      // log(ns, "Launching prepper for " + server + ".")
-      //ns.print("Server " + server + " maxMoney: " + ns.getServerMaxMoney(server) + ", availableMoney: " + ns.getServerMoneyAvailable(server))
-      successful = await launch_child(ns, "/scripts/manage_server_prep_v3.js", server)
-      if (!successful) {
-        // log(ns, "ERROR Failed to launch prepper for " + server + ".")
-      }
-      break
-    }
-  }
-
-  // log(ns, "Finished requesting new manager processes")
-  return Promise.resolve()
-}
-
-/**
  * 
  * @param {import("@ns").Server} mock 
  * @param {ServerStateInfo} server_info 
@@ -443,6 +268,9 @@ async function check_prep_managers(ns, process_info, control_params, bitnode_mul
       // Negative Result means a before b
       // Zero Result means no change
       // Positive Result means b before a
+      if (a === control_params.hacknet_mgr.hash_target) {return -1} // Sort the hash target to the top of the
+      if (b === control_params.hacknet_mgr.hash_target) {return  1} // Prepping Queue
+
       let mock_a = mock_server(ns.formulas.mockServer(), server_info[a])
       let mock_b = mock_server(ns.formulas.mockServer(), server_info[b])
 
@@ -458,7 +286,7 @@ async function check_prep_managers(ns, process_info, control_params, bitnode_mul
   while (num_to_launch > 0) {
     // Launch a new prepper
     let new_target = consider_prep.shift()
-    let success = await process_info.start_child_process(ns, "/src/scripts/manage_server_prep_v3.js", new_target)
+    let success = await process_info.start_child_process(ns, "/src/scripts/manage_server_prep_v4.js", new_target)
     if (!success) {consider_prep.push(new_target)}
     else {num_to_launch -= 1}
   }
@@ -516,15 +344,7 @@ async function check_hack_managers(ns, process_info, control_params, bitnode_mul
   let best_candidate = consider_hack.shift()
   if (best_candidate === undefined) {return}
 
-  
-
-  while (num_to_launch > 0) {
-    // Launch a new prepper
-    let new_target = consider_prep.shift()
-    let success = await process_info.start_child_process(ns, "/src/scripts/manage_server_prep_v3.js", new_target)
-    if (!success) {consider_prep.push(new_target)}
-    else {num_to_launch -= 1}
-  }
+  let success = await process_info.start_child_process(ns, "/src/scripts/manage_server_hack_v4.js", best_candidate)
 }
 
 /** @param {import("@ns").NS} ns */
